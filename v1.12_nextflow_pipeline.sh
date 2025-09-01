cd21bb7df62142487950fc35bdf885a939c569d4
Andrew Duncan <duncanaj@student.unimelb.edu.au>
2025-09-01 16:25:32 +1000

    Update v1.12_nextflow_pipeline.sh
    clarify outdir v1.12

--- v1.12_nextflow_pipeline.sh
+++ v1.12_nextflow_pipeline.sh
@@ -9,7 +9,7 @@ set -euo pipefail
 IN_DIR="/raid/VIDRL-USERS/HOME/aduncan/projects/nf-pipeline/classif_cases"
 REFERENCE_DIR="/raid/VIDRL-USERS/HOME/aduncan/projects/nf-pipeline/references"
 SCRUBBY_DIR="/raid/VIDRL-USERS/HOME/aduncan/projects/nf-pipeline/classif_cases/scrubby_clean"
-OUTDIR="/raid/VIDRL-USERS/HOME/aduncan/projects/nf-pipeline/classif_cases/nextflow_output"
+OUTDIR="/raid/VIDRL-USERS/HOME/aduncan/projects/nf-pipeline/classif_cases/standard_nextflow_output"
 REFERENCE_DIR="/raid/VIDRL-USERS/HOME/aduncan/projects/nf-pipeline/references"
 THREADS=16
 SCRUBBY_INDEX="${REFERENCE_DIR}/controls.fasta"
