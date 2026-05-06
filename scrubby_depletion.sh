ea2669126fa51deb43bc1c882a8c698342a93f51
Andrew Duncan <duncanaj@student.unimelb.edu.au>
2026-05-06 12:18:52 +1000

    Update scrubby_depletion.sh
    fix outdoor, aligner

--- scrubby_depletion.sh
+++ scrubby_depletion.sh
@@ -4,14 +4,12 @@ set -euo pipefail
 REFERENCE_DIR="/raid/VIDRL-USERS/HOME/aduncan/projects/nf-pipeline/references"
 THREADS=32
 SCRUBBY_INDEX="${REFERENCE_DIR}/controls.fasta"
-
 INDIR="$HOME/projects/ground_truth/naive_vs_Th1/fastqs"
-SCRUBBY_DIR="scrubby_clean"
+SCRUBBY_DIR="${INDIR}/scrubby_clean"
 
 echo "[Scrubby] Depleting synthetic controls using Scrubby"
 
 mkdir -p "$SCRUBBY_DIR"
-
 cd "$INDIR"
 
 shopt -s nullglob  # prevents literal glob issue
@@ -30,11 +28,11 @@ for r1 in *RNA__PS_*R1_001.fastq.gz; do
     scrubby reads \
         -i "$r1" -i "$r2" \
         --index "$SCRUBBY_INDEX" \
-        --aligner minimap2 \
+        --aligner bowtie2   # or bwa, depending on what scrubby supports
         --threads "$THREADS" \
         -o "${SCRUBBY_DIR}/${sample}__clean__R1.fq.gz" \
         -o "${SCRUBBY_DIR}/${sample}__clean__R2.fq.gz" \
         --json "${SCRUBBY_DIR}/${sample}.clean.json"
 done
 
-echo "Depletion of EDCC / ERCC controls complete"
+echo "Depletion of ERCC controls complete"
